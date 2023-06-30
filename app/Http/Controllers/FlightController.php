5e99579d11cd909df332984c386351c662633c05
Giovanni Ledda <ledda@netseven.it>
2023-06-30 16:02:10 +0200

    chore: modified readme

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -19,13 +19,13 @@ class FlightController extends Controller
 
     public function search(FlightRequest $request, FlightScannerService $flightScanner)
     {
-
         $departureCode = $request->get('depCode');
 
         $arrivalCode = $request->get('arrCode');
 
         $bestPrice = $flightScanner->generalMinPriceOptimizedSearch($departureCode, $arrivalCode);
 
+        // TODO: will be good to handle pagination, but not for now!
         $flightsData = $flightScanner->aggregateDataForApi($departureCode, $arrivalCode);
 
         return response()->view('flights.search-results', compact('flightsData', 'departureCode', 'arrivalCode', 'bestPrice'));
